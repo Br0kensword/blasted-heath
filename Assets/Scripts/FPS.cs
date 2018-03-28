ec8a4835b7b6d4d6846e751277564af7c8fa72af
Br0kensword <longislandhustler@gmail.com>
2018-03-28 11:30:59 -0700

    first commit of environment

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class FPS : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float camSens;
+    public float maxVert = 45f;
+    public float minVert = -45f;
+    private float gravity = 14.0f;
+    public GameObject cam;
+
+
+    private CharacterController player;
+
+
+    public void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        float moveVerticle = Input.GetAxis("Vertical") * moveSpeed;
+
+        float rotateHorizontal = Input.GetAxis("Mouse X") * camSens;
+        float rotateVerticle = Input.GetAxis("Mouse Y") * camSens;
+
+        Vector3 movement = new Vector3(moveHorizontal, -gravity, moveVerticle);
+
+        transform.Rotate(0, rotateHorizontal, 0);
+
+        rotateVerticle = Mathf.Clamp(rotateVerticle, minVert, maxVert);
+        cam.transform.Rotate(-rotateVerticle, 0, 0);
+
+        movement = transform.rotation * movement;
+
+        player.Move(movement * Time.deltaTime);
+
+    }
+}
